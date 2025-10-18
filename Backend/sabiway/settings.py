b437bf624a1e852a82ee96db939ad4750d805d17
ChiAde Tech <chiadetech@gmail.com>
2025-10-18 04:25:41 +0100

    Update Django backend

--- Backend/sabiway/settings.py
+++ Backend/sabiway/settings.py
@@ -51,8 +51,8 @@ BACKEND_URL = os.getenv("BACKEND_URL", "https://sabiway-9wq4.onrender.com")
 FRONTEND_URL = os.getenv("FRONTEND_URL", "https://sabiway2025.vercel.app")
 
 
-BACKEND_URL = os.getenv("BACKEND_URL", "https://sabiway-9wq4.onrender.com")
-FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
+# BACKEND_URL = os.getenv("BACKEND_URL", "https://sabiway-9wq4.onrender.com")
+# FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
 
 # import os
 
