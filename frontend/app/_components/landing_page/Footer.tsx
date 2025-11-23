e0ddd21c4d98bbd6e1ab38d754b9a2274f471a8d
ChiAde Tech <chiamakanwankwo557@gmail.com>
2025-11-23 15:49:49 +0000

    Some Adjustments done

--- frontend/app/_components/landing_page/Footer.tsx
+++ frontend/app/_components/landing_page/Footer.tsx
@@ -7,23 +7,18 @@ export default function Footer() {
       <div className="max-w-[2000px] mx-auto text-left">
         {/* Navigation Links */}
         <div className="flex flex-col gap-6">
-          <div className="flex flex-wrap justify-between gap-x-8 gap-y-3 text-sm font-medium">
+          <div className="flex flex-col md:flex-row flex-wrap justify-between gap-x-8 gap-y-5 text-sm font-medium">
             <a href="#">Home</a>
             <a href="#">About Us</a>
-            <a href="#">For Clients</a>
-            <a href="#">For Service Providers</a>
             <a href="#">FAQ</a>
-            <a href="#">Contact Us</a>
             <a href="#">Help Center</a>
             <a href="#">Privacy Policy</a>
             <a href="#">Terms of Service</a>
-          </div>
-
-          <div className="flex flex-wrap justify-start gap-x-10 gap-y-3 text-sm font-medium">
             <a href="#">Instagram</a>
             <a href="#">Facebook</a>
             <a href="#">Report a Problem</a>
           </div>
+
         </div>
 
         <div className="border-t border-white/20 my-8"></div>
