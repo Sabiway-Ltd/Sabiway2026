b84dfcc2781cd51a59b08f19fb198566e9644cda
ChiAde Tech <chiadetech@gmail.com>
2025-11-23 06:23:21 +0100

    Full Stack Flask Next Waitlist containized inside docker

---
+++ PORTS.md
@@ -0,0 +1,4 @@
+3000 - Sabiway Website Next JS
+4000 - Sabiway Waitlist Flask
+5000 - Sabiway Express JS
+8000 - Sabiway Django
--- WaitList/app.py
+++ WaitList/app.py
@@ -1,3 +1,4 @@
+# WaitList/app.py
 import os
 import threading
 import io
@@ -260,4 +261,4 @@ def admin():
 # Run
 # -----------------------------
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=4000)
