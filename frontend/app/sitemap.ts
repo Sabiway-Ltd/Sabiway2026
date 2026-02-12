4d8063e2e6d2dd854b3e69663da1047e69018706
ChiAde Tech <chiadetech@gmail.com>
2026-02-12 16:58:17 +0100

    Doing SEO for Sabi forum

---
+++ frontend/app/sitemap.ts
@@ -0,0 +1,13 @@
+// app/sitemap.ts
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.sabiway.com/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.sabiway.com/community",
+      lastModified: new Date(),
+    },
+  ];
+}
