ec0086b658c15f9f75d8419cdfb104de0f3f428e
Olaoluwa J. Taiwo <taiwo.olaoluwaj@gmail.com>
2026-08-17 00:05:55 +0100

    Phase 8: configure trust operations app

---
+++ Backend/trustops/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class TrustOpsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "trustops"
+    verbose_name = "Trust & Safety Operations"
