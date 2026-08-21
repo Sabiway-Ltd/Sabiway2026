df022ee05391f9d89a92dadf66f1abd129e2aff6
Olaoluwa J. Taiwo <taiwo.olaoluwaj@gmail.com>
2026-08-21 19:03:45 +0100

    test: include Phase 14 reputation authority in marketplace journeys

---
+++ Backend/marketplace/test_phase14_reputation.py
@@ -0,0 +1,7 @@
+from reputation.tests import ProfessionalReviewAuthorityTests
+
+
+class Phase14CompletedWorkReputationTests(ProfessionalReviewAuthorityTests):
+    """Run Phase 14 review authority as part of the required marketplace CI suite."""
+
+    pass
